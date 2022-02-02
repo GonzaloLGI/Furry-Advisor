d9ffcfb9e30ece3c9811012da6eea9d9ea375276
pusilanime110 <gpsonz4l0@gmail.com>
2022-02-02 16:32:18 +0100

    Initialized web project

---
+++ furry-advisor-web/src/main/java/com/example/demo/FurryAdvisorApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurryAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurryAdvisorApplication.class, args);
+	}
+
+}
