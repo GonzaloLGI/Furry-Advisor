4eb901aa18b4f9c473aa64cd2f502588296ee576
pusilanime110 <gpsonz4l0@gmail.com>
2022-03-15 19:50:42 +0100

    REST services for Deal incorporated (?) and JSON support
    I don't know if it will work, haven't used it properly yet

--- furry-advisor-web/src/main/java/com/example/demo/Entidades/DealDB.java
+++ furry-advisor-web/src/main/java/com/example/demo/Entidades/DealDB.java
@@ -11,24 +11,33 @@ import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonView;
 
 //Clase de la entidad Deal en la BD
 @Entity
 public class DealDB {
 	
+	public interface Basico{}
+	public interface Completo{}
+	
 	@Id
+	@JsonView(Basico.class)
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int deal_id;
 	
+	@JsonView(Basico.class)
 	@Column(nullable=false)
 	private String header;
 	
+	@JsonView(Basico.class)
 	private String description;
 	
+	@JsonView(DealDB.Completo.class)
 	@Lob
 	@JsonIgnore
 	private Blob dealPic;
 	
+	@JsonView(DealDB.Completo.class)
 	@ManyToOne
 	private PlaceDB placeOrigin;
 	
@@ -39,6 +48,14 @@ public class DealDB {
 		setHeader(h);
 		description = desc;
 		dealPic = photo;
+	}
+
+	public int getDeal_id() {
+		return deal_id;
+	}
+
+	public void setDeal_id(int deal_id) {
+		this.deal_id = deal_id;
 	}
 
 	public String getHeader() {
---
+++ furry-advisor-web/src/main/java/com/example/demo/Services/DealRest.java
@@ -0,0 +1,71 @@
+package com.example.demo.Services;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.demo.Entidades.DealDB;
+import com.example.demo.Entidades.PlaceDB;
+import com.example.demo.Interfaces.DealDBInterface;
+import com.fasterxml.jackson.annotation.JsonView;
+
+@RestController
+public class DealRest {
+	
+	interface DealComplete extends DealDB.Basico,DealDB.Completo{}
+	
+	@Autowired
+	private DealDBInterface dealRepository;
+	
+	@JsonView(DealDB.Basico.class)
+	@GetMapping("/getDealByHeader/{header}")
+	public ResponseEntity<List<DealDB>> findByHeader(@PathVariable String header){
+		List<DealDB> deals = dealRepository.findByHeader(header);
+		if(deals!=null) {
+			return ResponseEntity.ok(deals);
+		}else {
+			return  ResponseEntity.notFound().build();
+		}
+	}
+	
+	@JsonView(DealDB.Basico.class)
+	@GetMapping("/getExistingDeal")
+	public ResponseEntity<List<DealDB>> findAllByPlaceOriginIsNotNull(){
+		List<DealDB> deals = dealRepository.findAllByPlaceOriginIsNotNull();
+		if(deals!=null) {
+			return ResponseEntity.ok(deals);
+		}else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@JsonView(DealDB.Basico.class)
+	@GetMapping("/getDealByPlace/{place}")
+	public ResponseEntity<List<DealDB>> findAllByPlaceOrigin(@PathVariable PlaceDB placeOrigin){
+		List<DealDB> deals = dealRepository.findAllByPlaceOrigin(placeOrigin);
+		if(deals!=null) {
+			return ResponseEntity.ok(deals);
+		}else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@JsonView(DealComplete.class)
+	@PostMapping("/addDeal/{deal}")
+	public ResponseEntity<DealDB> save(@RequestBody DealDB deal){
+		dealRepository.save(deal);
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(deal.getDeal_id()).toUri();
+		return ResponseEntity.created(location).body(deal);
+	}
+}
