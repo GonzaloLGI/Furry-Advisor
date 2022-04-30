cf8523408340e183c9d686dab9f8f6291a24d057
dexaxi <64601602+CarlosMariano05@users.noreply.github.com>
2022-04-30 20:25:07 +0200

    cosas funcionando

--- furry-advisor-web/src/main/java/com/example/demo/FurryAdvisorApplication.java
+++ furry-advisor-web/src/main/java/com/example/demo/FurryAdvisorApplication.java
@@ -2,10 +2,12 @@ package com.example.demo;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
 
 @SpringBootApplication
 public class FurryAdvisorApplication {
@@ -13,15 +15,6 @@ public class FurryAdvisorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FurryAdvisorApplication.class, args);
 	}
-	
-	@Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource ds = new DriverManagerDataSource();
-        ds.setDriverClassName(com.mysql.jdbc.Driver.class.getName());
-        ds.setUrl("jdbc:mysql://localhost:3306/posts");
-        ds.setUsername("root");
-        ds.setPassword("nuevacontraseñafurry");
-        return ds;
-    }
+
 
 }
