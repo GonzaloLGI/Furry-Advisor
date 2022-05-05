82848679f757ef11596a8e547de731d79743f6d2
Javaperwave <javibg2000@gmail.com>
2022-05-05 12:25:25 +0200

    cache controller fixed

--- furry-advisor-web/src/main/java/com/example/demo/Controladores/CacheController.java
+++ furry-advisor-web/src/main/java/com/example/demo/Controladores/CacheController.java
@@ -1,3 +1,13 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Map;
 
 @RestController
