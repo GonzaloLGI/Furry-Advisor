87a211803577dfc6d58fd375fa44e6b97f818cac
Javaperwave <javibg2000@gmail.com>
2022-05-09 16:59:44 +0200

    Missing Import 3

--- furry-advisor-web/src/main/java/com/example/demo/Controladores/CacheController.java
+++ furry-advisor-web/src/main/java/com/example/demo/Controladores/CacheController.java
@@ -1,9 +1,9 @@
 package com.example.demo.Controladores;
 
+import com.hazelcast.spring.cache.HazelcastCacheManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
